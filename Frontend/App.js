feb4398899bf261eb480ea7fc5772990e0d94962
amrutabargal <bargal442001@gmail.com>
2025-11-18 14:33:16 +0530

    add

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import AuthProvider from './src/context/AuthContext';
-import RootNavigator from './src/navigation/RootNavigator';
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { SafeAreaProvider } from 'react-native-safe-area-context';
+// import AuthProvider from './src/context/AuthContext';
+// import RootNavigator from './src/navigation/RootNavigator';
+
+// export default function App() {
+//   return (
+//     <SafeAreaProvider>
+//       <AuthProvider>
+//         <NavigationContainer>
+//           <RootNavigator />
+//         </NavigationContainer>
+//       </AuthProvider>
+//     </SafeAreaProvider>
+//   );
+// }
+
+
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AuthProvider from "./src/context/AuthContext";
+import RootNavigator from "./src/navigation/RootNavigator";
 
 export default function App() {
   return (
