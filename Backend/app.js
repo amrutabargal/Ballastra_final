cb7774fb8604970549c5186ac613dbdfa7031a46
amrutabargal <bargal442001@gmail.com>
2025-11-18 12:46:01 +0530

    add

--- Backend/app.js
+++ Backend/app.js
@@ -11,4 +11,7 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
